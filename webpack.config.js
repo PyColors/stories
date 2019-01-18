74d1bee3a2ffa65cc02b76e56fe8a147c5346b1c
PyColors <patweb44@gmail.com>
2019-01-18 14:31:58 +0000

    Include JavaScript for a component and Build Assets for Production

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,45 @@
 const autoprefixer = require('autoprefixer');
 
-module.exports = [
-    {
-        entry: "./app.scss",
-        output: {
-            // This is necessary for webpack to compile
-            // But we never use style-bundle.js
-            filename: "style-bundle.js"
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.scss$/,
-                    use: [
-                        {
-                            loader: "file-loader",
-                            options: {
-                                name: "bundle.css"
-                            }
+module.exports = {
+    entry: ['./app.scss', './app.js'],
+    output: {
+        filename: 'bundles/bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: 'bundles/bundle.css',
                         },
-                        {loader: 'extract-loader'},
-                        {loader: 'css-loader'},
-                        {
-                            loader: 'postcss-loader',
-                            options: {
-                                plugins: () => [autoprefixer()]
-                            }
+                    },
+                    {loader: 'extract-loader'},
+                    {loader: 'css-loader'},
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            plugins: () => [autoprefixer()],
                         },
-                        {
-                            loader: 'sass-loader',
-                            options: {
-                                includePaths: ['./node_modules']
-                            }
+                    },
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            includePaths: ['./node_modules'],
                         },
-                    ]
-                }
-            ]
-        }
-    }
-];
+                    }
+                ],
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015'],
+                    plugins: ['transform-object-assign']
+                },
+            }
+        ],
+    },
+};
