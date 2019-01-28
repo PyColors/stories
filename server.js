c5b669711ff0322a433fbabdd79d9ebc1942c3af
PyColors <patweb44@gmail.com>
2019-01-28 21:01:35 +0000

    Debugging Options (debug and morgan)

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 var express = require('express');
 var chalk = require('chalk');
+var debug = require('debug')('server');
+var morgan = require('morgan');
 
 var app = express();
+
+app.use(morgan('tiny'));
 
 app.get('/', function (req, res) {
     res.send('coco');
 })
 
 app.listen(3000, function () {
-    console.log('listening on port ' + chalk.green('3000'));
+    debug(`listening on port ${chalk.green('3000')}`);
 });
