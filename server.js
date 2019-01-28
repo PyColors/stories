5a9e85de6e13f26885874e1251451e1c88b6eeee
PyColors <patweb44@gmail.com>
2019-01-28 23:15:08 +0000

    Static Files - Node Modules

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var path = require('path');
 var app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, 'views/index.html'));
