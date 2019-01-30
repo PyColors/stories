ccd5dc81117dac22f072c31ad9925907e2ed9e53
PyColors <patweb44@gmail.com>
2019-01-30 22:01:30 +0000

    Setting up Nodemon

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -16,6 +17,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
-app.listen(3000, () => {
-    debug(`listening on port ${chalk.green('3000')}`);
+app.listen(port, () => {
+    debug(`listening on port ${chalk.green(port)}`);
 });
