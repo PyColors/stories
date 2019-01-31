5d514e7b5d50651298e136f0a0d3d795e5c8dd3e
PyColors <patweb44@gmail.com>
2019-01-31 00:33:29 +0000

    Setting up EJS Templating Engine

--- server.js
+++ server.js
@@ -12,9 +12,11 @@ app.use(express.static(path.join(__dirname, '/public')));
 app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
 app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views/index.html'));
+  res.render('index');
 });
 
 app.listen(port, () => {
