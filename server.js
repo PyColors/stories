31270a21965f6b2f386124cfad2fab772e06d0e6
PyColors <patweb44@gmail.com>
2019-02-02 21:24:08 +0000

    Setting up Routing

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const bookRouter = express.Router();
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public')));
@@ -15,8 +16,27 @@ app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist'))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+bookRouter.route('/')
+    .get((req, res) => {
+        res.send('hello books');
+    })
+
+bookRouter.route('/single')
+    .get((req, res) => {
+        res.send('hello single book');
+    })
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Stories' });
+  res.render(
+  'index',
+   {
+    nav: [
+    { link: '/books', title: 'Books' },
+    {link: "/authors", title: "Authors"}],
+    title: 'Stories'
+    }
+   );
 });
 
 app.listen(port, () => {
