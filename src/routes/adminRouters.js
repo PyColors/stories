6cd71e7a95577c9e7ecb9a268198bd107f2010e3
PyColors <patweb44@gmail.com>
2019-02-03 16:40:25 +0000

    Setting up MongoDB : Testing Insert

--- src/routes/adminRouters.js
+++ src/routes/adminRouters.js
@@ -1,11 +1,58 @@
 const express = require('express');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('server:adminRoutes');
 
 const adminRouter = express.Router();
+const books = [
+    {
+        userId: 1,
+        id: 1,
+        title: "quidem molestiae enim"
+    },
+    {
+        userId: 1,
+        id: 2,
+        title: "sunt qui excepturi placeat culpa"
+    },
+    {
+        userId: 1,
+        id: 3,
+        title: "omnis laborum odio"
+    },
+    {
+        userId: 1,
+        id: 4,
+        title: "non esse culpa molestiae omnis sed optio"
+    },
+    {
+        userId: 1,
+        id: 5,
+        title: "eaque aut omnis a"
+    }
+];
 
 function router(nav) {
     adminRouter.route('/')
         .get((req, res) => {
-            res.send('inserting books');
+            const url = 'mongodb://localhost:27017';
+            const dbName = 'libraryApp';
+
+            (async function mongo() {
+                let client;
+                try {
+                    client = await MongoClient.connect(url);
+                    debug('Connected correctly to server');
+
+                    const db = client.db(dbName);
+
+                    const response = await db.collection('books').insertMany(books);
+                    res.json(response);
+                } catch (err) {
+                    debug(err.stack);
+                }
+
+                client.close();
+            }());
         });
 
     return adminRouter;
