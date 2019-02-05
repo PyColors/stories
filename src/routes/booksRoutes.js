2cc546dfdeab2a21a81b4f99774490d61eb7ab20
PyColors <patweb44@gmail.com>
2019-02-05 21:07:34 +0000

    Databases : Select One with MongoDB

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -1,38 +1,10 @@
 const express = require('express');
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 const debug = require('debug')('server:bookRoutes');
 
 const bookRouter = express.Router();
 
 function router (nav) {
-
-    const books = [
-        {
-            userId: 1,
-            id: 1,
-            title: "quidem molestiae enim"
-        },
-        {
-            userId: 1,
-            id: 2,
-            title: "sunt qui excepturi placeat culpa"
-        },
-        {
-            userId: 1,
-            id: 3,
-            title: "omnis laborum odio"
-        },
-        {
-            userId: 1,
-            id: 4,
-            title: "non esse culpa molestiae omnis sed optio"
-        },
-        {
-            userId: 1,
-            id: 5,
-            title: "eaque aut omnis a"
-        }
-    ];
 
     bookRouter.route('/')
         .get((req, res) => {
@@ -69,14 +41,34 @@ function router (nav) {
     bookRouter.route('/:id')
         .get((req, res) => {
             const {id} = req.params;
-            res.render(
-                'bookView',
-                {
-                    nav,
-                    title: 'Stories',
-                    book: books[id]
+            const url = 'mongodb://localhost:27017';
+            const dbName = 'libraryApp';
+
+            (async function mongo (){
+                let client;
+                try {
+                    client = await MongoClient.connect(url);
+                    debug('Connected correctly to server');
+
+                    const db = client.db(dbName);
+
+                    const col = await db.collection('books');
+
+                    const book = await col.findOne({_id: new ObjectID(id)});
+                    debug(book);
+
+                    res.render(
+                        'bookView',
+                        {
+                            nav,
+                            title: 'Stories',
+                            book
+                        }
+                    );
+                } catch (err) {
+                    debug(err.stack);
                 }
-            );
+            }());
         });
 
     return bookRouter;
