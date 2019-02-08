a767f70930634a2c465027703b41994023343e24
Parny Patrice <patweb44@gmail.com>
2019-02-08 21:54:38 +0000

    Setting up authentication : Creating a User with MongoDB ✎

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,14 +7,36 @@ const authRouter = express.Router();
 function router() {
     authRouter.route('/signUp')
         .post((req, res) => {
-            debug(req.body);
+            const { username, password } = req.body;
+            const url = 'mongodb://localhost:27017';
+            const dbName = 'libraryApp';
 
-            // Create a user
-            // login comes from to passport.initialize()
-            req.login(req.body, () => {
-                res.redirect('/auth/profile');
-            });
-       //     res.json(req.body);
+            (async function addUser() {
+                let client;
+                try {
+                    client = await MongoClient.connect(url);
+                    debug('Connected correctly to the server');
+
+                    // connect to database
+                    const db = client.db(dbName);
+
+                    // connect to user collection
+                    const col = db.collection('users');
+                    const user = { username, password };
+                    const results = await col.insertOne(user);
+                    debug(results);
+
+                    /* Create a user */
+                    // login comes from to passport.initialize()
+                    // ops[] is the user from debug console
+                    req.login(results.ops[0], () => {
+                        res.redirect('/auth/profile');
+                    });
+
+                } catch (err) {
+                    debug(err);
+                }
+            }());
         });
 
     authRouter.route('/profile')
