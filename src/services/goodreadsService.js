7d1faa2cd7267aa89a7788437173e2ca5225429d
Parny Patrice <patweb44@gmail.com>
2019-02-10 15:59:26 +0000

    Making API Call ➤

--- src/services/goodreadsService.js
+++ src/services/goodreadsService.js
@@ -1,8 +1,29 @@
+const axios = require('axios');
+const xml2js = require('xml2js');
+const debug = require('debug')('server:goodreadsService');
+
 function goodreadsService() {
 
-    function getBookById() {
+    // By default 'xml2js' put an Array for everything
+    const parser = xml2js.Parser({explicitArray: false});
+
+    function getBookById(id) {
         return new Promise((resolve, reject) => {
-            resolve({description: 'some description'});
+            axios.get('https://www.goodreads.com/book/show/50.xml?key=X4ePRnKnYFn9BqXV4srqzw')
+                .then((response) => {
+                    parser.parseString(response.data, (err, result) => {
+                        if (err) {
+                            debug(err);
+                        } else {
+                            debug(result);
+                            resolve(result.GoodreadsResponse.book);
+                        }
+                    })
+                })
+                .catch((error) => {
+                    reject(error);
+                    debug(error);
+                });
         });
     }
 
