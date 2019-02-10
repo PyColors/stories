a198cb758e86ccb9d80b7961141f8e3ea2c27073
Parny Patrice <patweb44@gmail.com>
2019-02-10 01:40:45 +0000

    Authentication - Setting up Validating User

--- src/config/strategies/local.stategy.js
+++ src/config/strategies/local.stategy.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 const { Strategy } = require('passport-local');
+const { MongoClient } = require('mongodb');
+const debug = require('debug')('server:local.strategy');
 
 module.exports = function localStrategy() {
     passport.use(new Strategy(
@@ -7,10 +9,34 @@ module.exports = function localStrategy() {
             usernameField: 'username',
             passwordField: 'password'
         }, (username, password, done) => {
-            const user = {
-             username, password
-            };
-            done(null, user)
-        }
-    ));
+            const url = 'mongodb://localhost:27017';
+            const dbName = 'libraryApp';
+
+            (async function addUser() {
+                let client;
+                try {
+                    client = await MongoClient.connect(url);
+                    debug('Connected correctly to the server');
+
+                    // connect to database
+                    const db = client.db(dbName);
+
+                    // connect to user collection
+                    const col = db.collection('users');
+
+                    const user = await col.findOne({username});
+
+                    if (user.password === password) {
+                        done(null, user);
+                    } else {
+                        done(null, false);
+                    }
+                } catch (err) {
+                    debug(err);
+                }
+                // Close connect
+                client.close();
+
+            }());
+        }));
 };
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { MongoClient, ObjectID } = require('mongodb');
+const { MongoClient } = require('mongodb');
 const debug = require('debug')('server:authRoutes');
 const passport = require('passport');
 
