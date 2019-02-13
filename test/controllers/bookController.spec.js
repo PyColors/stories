f0e030d28ded95fa602a60a414eb5f6423686441
PyColors <patweb44@gmail.com>
2019-02-13 21:25:33 +0000

    Setting up Mocha test

---
+++ test/controllers/bookController.spec.js
@@ -0,0 +1,16 @@
+const chai = require("chai");
+// const expect = chai.expect;
+
+const indexPage = require("/src/controllers/bookController");
+
+describe("getIndex", function () {
+    it("should return index page function", function () {
+        let req = {}
+        let res = {
+            send: function () {
+            }
+        }
+
+        indexPage.getIndex(req, res)
+    });
+});
