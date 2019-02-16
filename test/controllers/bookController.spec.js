b99ba0cc6aa95c819351ea758e4c80ed80b9a0f4
PyColors <patweb44@gmail.com>
2019-02-16 23:00:14 +0000

    Adding Sinon and Chai

--- test/controllers/bookController.spec.js
+++ test/controllers/bookController.spec.js
@@ -5,11 +5,11 @@ const indexPage = require("../../src/controllers/bookController");
 
 describe("getIndex", function () {
     it("should return index page function", function () {
-        let req = {}
+        let req = {};
         let res = {
             send: function () {
             }
-        }
+        };
 
         indexPage.getIndex(req, res)
     });
